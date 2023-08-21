74feb076dd8e0d1e8f2070a29933907c2f9cdee4
Ira Bell <ira@irabell.com>
2023-08-21 18:56:50 -0500

    Update transits.py

--- formulas/transits.py
+++ formulas/transits.py
@@ -1,6 +1,6 @@
 import math
 
-# Brightness Drop = Brightness Before Transit - Brightness During Transit
+# Calculate Area
 def area(x=0):
 	radius = x
 
@@ -10,7 +10,7 @@ result = area(10)
 print(f'The area of the planet or star is {result}.')
 
 
-
+# Brightness Drop = Brightness Before Transit - Brightness During Transit
 def brightness_drop(x=0, y=1):
 	brightness_before_transit = x
 	brightness_during_transit = y
@@ -40,3 +40,16 @@ def percent_brightness_drop_area(x=0, y=1):
 
 result = percent_brightness_drop_area(10000000,30)
 print(f'The Brightness Drop using the star and planet area is {result}%')
+
+# % Brightness Drop using Radius of Star and Planet
+def percent_brightness_drop_radius(x=0, y=1):
+	radius_star = x
+	radius_planet = y
+
+	area_star = math.pi * (radius_star ** 2)
+	area_planet = math.pi * (radius_planet ** 2)
+
+	return (area_planet/area_star) * 100
+
+result = percent_brightness_drop_radius(100000,34)
+print(f'The Brightness Drop usiing the radius of a star and the radius of a planet is {result}%.')
