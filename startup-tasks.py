79fa33e2798f98c6570475e2318986829881fed0
Ira Bell <ira@irabell.com>
2023-08-21 17:55:15 -0500

    Create startup-tasks.py

---
+++ startup-tasks.py
@@ -0,0 +1,6 @@
+# startup tasks (this is not a python file, per say...)
+# rather, it's a reference document for things I need to run
+# from the command line often
+
+# To Create a Virtual Environment in Python
+python -m venv /path/to/new/vitual/environment
